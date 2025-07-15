1f70705927719e2d1a2fca795ff7dba611015cf5
Jon <66891482+jonzepp21@users.noreply.github.com>
2025-07-15 01:38:27 -0700

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,32 +1,18 @@
 from gpiozero import Button
 from time import sleep
 
-button4 = Button(4)# used Stats
-button17 = Button(17) #used Items
-button18 = Button(18)
-button27 = Button(27) #used MAP
-button22 = Button(22)
-#button23 = Button(23) Reboots system if called pin 16
-#button24 = Button(24) No edge detection pin 18
-button25 = Button(25)
-while True:
+clk = Button(17, pull_up=True)
+dt = Button(27, pull_up=True)
+#sw = Button(22, pull_up=True)
+
+def dial_up():
+    trigger_action("dial_up")
+    sleep(0.002)
     
-    if button4.is_pressed:
-        print("4 Pressed pin7")
-    elif button17.is_pressed:
-        print("17 Pressed pin 11")
-    elif button18.is_pressed:
-        print("18 Pressed pin 12")
-    elif button27.is_pressed:
-        print("27 Pressed pin 13")
-    elif button22.is_pressed:
-        print("22 Pressed pin 15")
-    #elif button23.is_pressed:
-    #    print("23 Pressed pin 16")
-    #elif button24.is_pressed:
-    #    print("24 Pressed pin 18")
-    elif button25.is_pressed:
-        print("25 Pressed pin 22")
-    else:
-        print("Released")
-    sleep(1)
+def dial_down():
+    trigger_action("dial_down")
+    sleep(0.002)
+
+button_dial_up.when_pressed = dial_up
+button_dial_down.when_pressed = dial_down
+
